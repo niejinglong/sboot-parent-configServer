e81a45396ab00d7ffc4a9c22db0b9da7ebd40ccc
niejinglong <296621041@qq.com>
2019-04-18 10:48:36 +0800

    增加configServer服务应用和服务配置文件

---
+++ sboot-parent-config-server/src/main/java/main/SbootConfigServer.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * sbootConfigServer 主程序
+ */
+@SpringBootApplication
+public class SbootConfigServer {
+    private static Logger logger = LoggerFactory.getLogger(SbootConfigServer.class);
+    public static void main(String[] args) {
+        logger.info("sbootConfigServer 主程序开始启动。。。。。");
+        SpringApplication.run(SbootConfigServer.class, args);
+        logger.info("sbootConfigServer 主程序结束启动。。。。。");
+    }
+}
