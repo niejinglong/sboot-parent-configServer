fa30581a55b3335dc312f5fa3b21247a55350648
niejinglong <296621041@qq.com>
2019-04-18 11:02:37 +0800

    增加服务配置文件

--- sboot-parent-config-server/src/main/java/main/SbootConfigServer.java
+++ sboot-parent-config-server/src/main/java/main/SbootConfigServer.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * sbootConfigServer 主程序
  */
 @SpringBootApplication
+@EnableConfigServer
 public class SbootConfigServer {
     private static Logger logger = LoggerFactory.getLogger(SbootConfigServer.class);
     public static void main(String[] args) {
