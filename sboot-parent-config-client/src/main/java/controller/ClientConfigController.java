501ed24c9c9e7f8a929cde856a45e28c841acb2b
niejinglong <296621041@qq.com>
2019-04-19 19:32:57 +0800

    configclient

--- sboot-parent-config-client/src/main/java/controller/ClientConfigController.java
+++ sboot-parent-config-client/src/main/java/controller/ClientConfigController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ConfigController {
+public class ClientConfigController {
+
     @Value("${profile}")
-    private String proflie;
+    private String profile;
 
     @GetMapping("/getpf")
-    public String getProfie(){
-        return proflie;
+    public String getProfile(){
+        return profile;
     }
 }
