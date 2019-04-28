5195ca39614c432c2fce7769e14978fa03e686c8
niejinglong <296621041@qq.com>
2019-04-28 15:58:35 +0800

    处理client

--- sboot-parent-config-client/src/main/java/app/controller/ClientConfigController.java
+++ sboot-parent-config-client/src/main/java/app/controller/ClientConfigController.java
@@ -1,4 +1,4 @@
-package controller;
+package app.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientConfigController {
-
     @Value("${profile}")
     private String profile;
 
