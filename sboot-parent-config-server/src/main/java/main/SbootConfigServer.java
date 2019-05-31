468725d4bbbe331b845c7b1fd17a20b964fa392c
niejinglong <296621041@qq.com>
2019-05-31 18:00:22 +0800

    增加服务安全认证处理

--- sboot-parent-config-server/src/main/java/main/SbootConfigServer.java
+++ sboot-parent-config-server/src/main/java/main/SbootConfigServer.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import javax.servlet.annotation.ServletSecurity;
+
 /**
  * sbootConfigServer 主程序
  */
